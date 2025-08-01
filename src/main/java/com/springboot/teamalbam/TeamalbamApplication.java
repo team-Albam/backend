f0da4b46bc4749b30eba64ea3c39e80adab3f084
SeungyeonWoo <seungyeon3287@gmail.com>
2025-08-01 11:26:34 +0900

    init: 프로젝트 생성 및 .gitignore 추가

---
+++ src/main/java/com/springboot/teamalbam/TeamalbamApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.teamalbam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamalbamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamalbamApplication.class, args);
+	}
+
+}
